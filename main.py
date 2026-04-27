cf1ed7b180287c8fd70b7cafed32bff36f3de30e
Jose Daniel <jose.huaman@utec.edu.pe>
2026-04-27 16:17:02 -0500

    Refactor main.py for external hashing and sorting

--- main.py
+++ main.py
@@ -52,3 +52,60 @@ for INDEX, BUFFER_SIZE in enumerate(BUFFER_SIZE_LIST):
     )
     print(f"====== TEST {INDEX} ======\n", result, "\n")
 """
+
+""" USO DEL EXTERNAL HASHING PARA HACER UN GROUP BY EN UN ARCHIVO QUE NO CABE EN RAM
+from heapfile import *
+
+heap_path = "heap"
+
+employee_path = "employee"
+department_path = "department_employee"
+
+# id:5s, birth:10s, first:30s, last:30s, gender:1s, hire:10s
+employee_format = "<5s10s30s30s1s10s"
+# id:5s, departmentid:4s, from:10s, to:10s
+department_format = "<5s4s10s10s"
+
+# employee table
+export_to_heap(
+    f"{employee_path}.csv",
+    f"{heap_path}/{employee_path}.dat",
+    employee_format,
+    4096
+)
+# employee department table
+export_to_heap(
+    f"{department_path}.csv",
+    f"{heap_path}/{department_path}.dat",
+    department_format,
+    4096
+)
+from external_hashing import external_hash_group_by
+
+print("\n" + "="*40)
+print(" INICIANDO PRUEBA DE EXTERNAL HASHING")
+print("="*40)
+
+# 2. Definimos los parámetros del laboratorio
+# Usaremos el archivo .dat que main.py acaba de crear en la carpeta heap
+archivo_a_procesar = f"{heap_path}/{department_path}.dat"
+tamano_pagina = 4096      # 4 KB por página
+memoria_ram = 65536       # 64 KB de buffer total en RAM
+resultados = external_hash_group_by(archivo_a_procesar, tamano_pagina, memoria_ram, "from_date")
+print("\n--- MUESTRA DE RESULTADOS ---")
+contador = 0
+for fecha_bytes, cantidad in resultados.items():
+    # Como los datos vienen en bytes desde el archivo binario, 
+    # los decodificamos a texto normal para que se vean bonitos
+    try:
+        fecha_texto = fecha_bytes.decode('utf-8').strip('\x00')
+    except:
+        fecha_texto = str(fecha_bytes)
+        
+    print(f"Fecha: {fecha_texto} ---> Se contrataron {cantidad} empleados")
+    
+    contador += 1
+    if contador >= 15: # Solo mostramos 15 para no saturar la pantalla
+        break
+
+"""
